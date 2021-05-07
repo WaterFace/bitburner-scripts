718a813ea6072422c96b733cb97f28095a9de277
WaterFace <kevinvisserweckworth@gmail.com>
2021-05-07 01:11:08 -0400

    finished `setup.js`, with prefix corresponding to this repository

--- setup.js
+++ setup.js
@@ -1,3 +1,8 @@
 export async function main(ns) {
   var files = ["wait-grow.js", "wait-hack.js", "wait-weak.js", "available-ram.js", "library.js", "botnet.js"];
+  var prefix = "https://raw.githubusercontent.com/WaterFace/bitburner-scripts/master/";
+
+  files.forEach(function(filename) {
+    ns.wget(prefix + filename, filename);
+  });
 }
