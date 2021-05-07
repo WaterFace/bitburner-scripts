644956ebc340813d7169610b2176dbcdf4b93fd6
WaterFace <kevinvisserweckworth@gmail.com>
2021-05-07 01:14:50 -0400

    fixed `setup.js` for real

--- setup.js
+++ setup.js
@@ -2,7 +2,7 @@ export async function main(ns) {
   var files = ["wait-grow.js", "wait-hack.js", "wait-weak.js", "available-ram.js", "library.js", "botnet.js"];
   var prefix = "https://raw.githubusercontent.com/WaterFace/bitburner-scripts/master/";
 
-  files.forEach(function(filename) {
+  for (const filename of files) {
     await ns.wget(prefix + filename, filename);
-  });
+  }
 }
